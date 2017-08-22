b440e0fbd2b2a918c8fd3dfa2a27fd35b3f80415
laurelnaiad <laurelnaiad@gmail.com>
2017-08-22 05:00:19 -0700

    fix(*): testing-induced fixes, publish prep
    * improve cli design/testability
    * fixes, fixes, fixes
    * --progress=bar:force in bash download
    * not so verbose when slow in node download
    * docs tweaks
    * travis config for npm publishing

--- src/cli/cli-build.ts
+++ src/cli/cli-build.ts
@@ -1,29 +1,26 @@
 #!/usr/bin/env node
 const { Command } = require('commander')
-const chalk = require('chalk')
-import {
-  handleSuccess,
-  handleError,
-  cliFollower,
-} from './handlers'
+import { cliFollower } from './handlers'
 import * as opts from './opts'
+import * as cli from './cli'
 import {
   MlDock,
   DevCreds,
 } from '../index'
 
-export  const program = new Command()
-
-export function cmdBuild(version: string, options: {
+export function buildCmd(version: string, options: {
   repo: string,
   rpmFile?: string,
   email?: string,
   password?: string,
   overwrite?: boolean
 }): Promise<string> {
-  if (!(options.rpmFile || (options.password && options.email))) {
+  if (
+    !(options.rpmFile || (options.password && options.email)) ||
+    options.rpmFile && (options.password || options.email)
+  ) {
     return Promise.reject(new Error(
-      'The `build` action requires at least the `rpmFile` or `email` and `password` options to be set.'
+      'The `build` action requires either the `rpmFile` or the `email` and `password` options to be set.'
     ))
   }
   const source = options.rpmFile ? options.rpmFile : <DevCreds>options
@@ -37,23 +34,20 @@ export function cmdBuild(version: string, options: {
   )
 }
 
-export function runProgram(args: string[]) {
-  program.parse(args)
-  if (!program.args[0]) {
-    program.outputHelp()
-    process.exit(1)
-  }
-  return cmdBuild(program.args[0], program)
-  .then(handleSuccess, handleError)
+export function buildProgram() {
+  const program = new Command()
+  program
+  .option(...opts.repo)
+  .option(...opts.rpmFile)
+  .option(...opts.email)
+  .option(...opts.password)
+  .option(...opts.overwriteImage)
+
+  return program
 }
 
-program
-.option(...opts.repo)
-.option(...opts.rpmFile)
-.option(...opts.email)
-.option(...opts.password)
-.option(...opts.overwriteImage)
-
-if (require.main === module) {
-  runProgram(process.argv)
-}
+cli.liftProgram(
+  buildProgram,
+  buildCmd,
+  module,
+)
