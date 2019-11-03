1c61e1ca035c0728ff5c71b102419e602be99b8f
laurelnaiad <laurelnaiad@gmail.com>
2019-11-03 17:25:51 -0500

    chore(ci): update travis settings for deployment
    Use new travis providers for deployment. This may take a few tries...

--- scripts/makePackage.js
+++ scripts/makePackage.js
@@ -10,3 +10,4 @@ delete pkg.nyc;
 fsx.writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2));
 fsx.writeFileSync('dist/README.md', fsx.readFileSync('./README.md').toString());
 fsx.writeFileSync('dist/LICENSE.txt', fsx.readFileSync('./LICENSE.txt').toString());
+fsx.removeSync('dist/.temp');
